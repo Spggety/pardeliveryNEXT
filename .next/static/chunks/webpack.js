33f11e07cc91371cb1e1bfe7e1d06212c1c2e664
Roman Lyashko <spaggetywork@gmail.com>
2026-04-07 11:23:48 +0300

    Удалил ненужные папки

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67593b5486cd040a"; }
+/******/ 		__webpack_require__.h = function() { return "9f50ceba0ace57d2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -84,7 +84,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b90e3873b89c661d"; }
+/******/ 	__webpack_require__.h = function() { return "551d269ecc79eefc"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
